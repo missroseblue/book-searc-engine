b6f5887baeb2d6e5fc8c1253d24fbd5ef3f3db8d
Sandra Caceres <missroseblue@gmail.com>
2022-01-09 18:41:06 -0800

    initial commit

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,25 +1,19 @@
-import gql from 'graphql-tag';
-
+import gql from "graphql-tag";
 
 export const GET_ME = gql`
-{
+  {
     me {
       _id
       username
       email
-      bookCount
       savedBooks {
-        # _id
+        title
         bookId
         authors
+        description
         image
         link
-        title
-        description
       }
     }
   }
 `;
-
-
-
